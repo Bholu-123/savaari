127fad8016f1c1361ac0b6dc96f9e906a3bab4a8
Bholu Tiwari <bholutiwari121@gmail.com>
2022-01-14 18:44:08 +0530

    removed class error

--- components/BookingDeals.js
+++ components/BookingDeals.js
@@ -4,23 +4,23 @@ import styles from '../styles/BookingDeals.module.css';
 const BookingDeals = () => {
     return (
       <div className={styles.container}>
-        <div class={styles.title}>
+        <div className={styles.title}>
           <h2>deals on booking</h2>
           <span className={styles.underline}></span>
         </div>
-        <div class={styles.dealsRow}>
-          <div class={styles.dealsColumn}>
+        <div className={styles.dealsRow}>
+          <div className={styles.dealsColumn}>
             <div className={styles.deals}>
-              <div class={styles.dealsContent}>
-                <div class={styles.dealsDetail}>
+              <div className={styles.dealsContent}>
+                <div className={styles.dealsDetail}>
                   <h3>marseille</h3>
                   <h2>
                     $102 <span> / per day</span>
                   </h2>
                 </div>
-                <img src="./barcode.png" alt="" class={styles.barcode} />
+                <img src="./barcode.png" alt="" className={styles.barcode} />
               </div>
-              <div class={styles.dealsImage}>
+              <div className={styles.dealsImage}>
                 <img src="./video-image.jpg" alt="" className={styles.image} />
               </div>
             </div>
@@ -28,16 +28,16 @@ const BookingDeals = () => {
 
           <div className={styles.dealsColumn}>
             <div className={styles.deals}>
-              <div class={styles.dealsContent}>
-                <div class={styles.dealsDetail}>
+              <div className={styles.dealsContent}>
+                <div className={styles.dealsDetail}>
                   <h3>marseille</h3>
                   <h2>
                     $102 <span> / per day</span>
                   </h2>
                 </div>
-                <img src="./barcode.png" alt="" class={styles.barcode} />
+                <img src="./barcode.png" alt="" className={styles.barcode} />
               </div>
-              <div class={styles.dealsImage}>
+              <div className={styles.dealsImage}>
                 <img src="./video-image.jpg" alt="" className={styles.image} />
               </div>
             </div>
@@ -45,16 +45,16 @@ const BookingDeals = () => {
 
           <div className={styles.dealsColumn}>
             <div className={styles.deals}>
-              <div class={styles.dealsContent}>
-                <div class={styles.dealsDetail}>
+              <div className={styles.dealsContent}>
+                <div className={styles.dealsDetail}>
                   <h3>marseille</h3>
                   <h2>
                     $102 <span> / per day</span>
                   </h2>
                 </div>
-                <img src="./barcode.png" alt="" class={styles.barcode} />
+                <img src="./barcode.png" alt="" className={styles.barcode} />
               </div>
-              <div class={styles.dealsImage}>
+              <div className={styles.dealsImage}>
                 <img src="./video-image.jpg" alt="" className={styles.image} />
               </div>
             </div>
