b09466add27d893b203752db6f2e98862ea89069
Bholu Tiwari <bholutiwari121@gmail.com>
2022-01-15 17:25:31 +0530

    commit apos changes

--- components/BookingDeals.js
+++ components/BookingDeals.js
@@ -63,25 +63,25 @@ const BookingDeals = () => {
         </div> */}
 
         <p>
-          We are Yatri Car Rental, an cab booking aggregator, providing
+          {`We are Yatri Car Rental an cab booking aggregator providing
           customers with reliable and premium Outstation and Local Car Rental
-          Service. Over the last decade, we are uniquely placed as the largest
+          Service. Over the last decade we are uniquely placed as the largest
           chauffeur driven car rental company in India in terms of geographical
           reach. Planning a weekend getaway? Our outstation cab services will
-          help you explore the best destinations, visit all the must-see places
-          and taste the best local food. Did you just land at an Airport or
-          Railway Station closest to your destination? No problem! You can use
+          help you explore the best destinations visit all the must-see places
+          and taste the best local food Did you just land at an Airport or
+          Railway Station closest to your destination No problem! You can use
           our Airport Taxi in Lucknow, Mumbai, Pune, Banglore, Varanasi
-          Kalyan, the transit pick up service to cover the last mile. We'll get
+          Kalyan, the transit pick up service to cover the last mile. We will get
           you to your destination and show you some of the best sights along the
           way. Decided to take a personal day and spend the whole day exploring
           your city? Our Car Rental packages will help you explore the best
-          places to eat and drink at, some of the city's majestic monuments,
-          greenest parks and oldest temples. You'll never have to worry about an
+          places to eat and drink at, some of the citys majestic monuments,
+          greenest parks and oldest temples. You will never have to worry about an
           empty travel itinerary again. Are you an offbeat traveller? Do you
           just hit the road and decide to take it from there? We offer one-way
           drops on several routes, in case you only want to be dropped to a
-          destination and don't want to look back.
+          destination and dont want to look back.`}
         </p>
       </div>
     );
